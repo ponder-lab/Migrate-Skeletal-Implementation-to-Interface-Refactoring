4684922ee3fc85e21e0972f5e60aecd893ee377d
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-06-04 16:46:14 +0900

    Add refactoring contribution.

---
+++ src/edu/cuny/citytech/defaultrefactoring/contributions/MigrateSkeletalImplementationToInterfaceRefactoringContribution.java
@@ -0,0 +1,19 @@
+package edu.cuny.citytech.defaultrefactoring.contributions;
+
+import java.util.Map;
+
+import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
+
+public class MigrateSkeletalImplementationToInterfaceRefactoringContribution
+		extends org.eclipse.ltk.core.refactoring.RefactoringContribution {
+
+	@Override
+	public RefactoringDescriptor createDescriptor(String id, String project,
+			String description, String comment,
+			@SuppressWarnings("rawtypes") Map arguments, int flags)
+			throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
