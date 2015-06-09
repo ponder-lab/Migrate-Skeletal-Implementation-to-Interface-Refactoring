62205e1c19429c6d09bbdf860be845342e232c54
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-06-09 17:20:31 +0900

    Fixed compilation errors.
    3: Complete setup 
    
    Task-Url:
    https://github.com/khatchad/Java-8-Interface-Refactoring/issues/issue/3

--- edu.cuny.citytech.defaultrefactoring/src/edu/cuny/citytech/defaultrefactoring/ui/MigrateSkeletalImplementationToInterfaceRefactoring.java
+++ edu.cuny.citytech.defaultrefactoring/src/edu/cuny/citytech/defaultrefactoring/ui/MigrateSkeletalImplementationToInterfaceRefactoring.java
@@ -5,6 +5,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -12,7 +13,7 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class MigrateSkeletalImplementationToInterfaceRefactoring extends org.eclipse.ltk.core.refactoring.Refactoring {
+public class MigrateSkeletalImplementationToInterfaceRefactoring extends Refactoring {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "edu.cuny.citytech.defaultrefactoring.ui"; //$NON-NLS-1$
@@ -26,24 +27,6 @@ public class MigrateSkeletalImplementationToInterfaceRefactoring extends org.ecl
 	public MigrateSkeletalImplementationToInterfaceRefactoring() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
 	/**
 	 * Returns the shared instance
 	 *
@@ -51,17 +34,6 @@ public class MigrateSkeletalImplementationToInterfaceRefactoring extends org.ecl
 	 */
 	public static MigrateSkeletalImplementationToInterfaceRefactoring getDefault() {
 		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
 
 	@Override
