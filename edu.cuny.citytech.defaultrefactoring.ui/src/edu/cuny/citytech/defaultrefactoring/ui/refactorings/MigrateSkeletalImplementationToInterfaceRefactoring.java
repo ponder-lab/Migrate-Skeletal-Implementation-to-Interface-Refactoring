23e51493ce936333dec1509813bd97a9467e2366
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-06-18 13:26:50 -0400

    Removed unused import.

--- edu.cuny.citytech.defaultrefactoring.ui/src/edu/cuny/citytech/defaultrefactoring/ui/refactorings/MigrateSkeletalImplementationToInterfaceRefactoring.java
+++ edu.cuny.citytech.defaultrefactoring.ui/src/edu/cuny/citytech/defaultrefactoring/ui/refactorings/MigrateSkeletalImplementationToInterfaceRefactoring.java
@@ -2,7 +2,6 @@ package edu.cuny.citytech.defaultrefactoring.ui.refactorings;
 
 import java.text.MessageFormat;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
