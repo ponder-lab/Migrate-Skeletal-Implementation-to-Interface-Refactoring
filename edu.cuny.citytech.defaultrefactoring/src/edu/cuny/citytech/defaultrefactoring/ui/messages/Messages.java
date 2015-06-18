c70ed44067a5834a62d8fb5ffe1742da5c670998
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-06-17 23:56:56 -0400

    Some progress on #4.

--- edu.cuny.citytech.defaultrefactoring/src/edu/cuny/citytech/defaultrefactoring/ui/messages/Messages.java
+++ edu.cuny.citytech.defaultrefactoring/src/edu/cuny/citytech/defaultrefactoring/ui/messages/Messages.java
@@ -11,17 +11,22 @@ import org.eclipse.osgi.util.NLS;
  */
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "edu.cuny.citytech.defaultrefactoring.ui.messages.messages"; //$NON-NLS-1$
-	
+
+	public static String MigrateSkeletalImplementationToInferfaceRefactoring_MethodsNotSpecified;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_Name;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_CheckingPreconditions;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_CompilingSource;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_CreatingChange;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_CUContainsCompileErrors;
-	public static String MigrateSkeletalImplementationToInferfaceRefactoring_FileDoesNotExist;
+	public static String MigrateSkeletalImplementationToInferfaceRefactoring_MethodDoesNotExist;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_PreconditionFailed;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_RefactoringNotPossible;
 	public static String MigrateSkeletalImplementationToInferfaceRefactoring_WrongType;
-	
+	public static String MigrateSkeletalImplementationToInferfaceRefactoring_CantChangeMethod;
+	public static String MigrateSkeletalImplementationToInferfaceRefactoring_NoConstructors;
+	public static String MigrateSkeletalImplementationToInferfaceRefactoring_NoAnnotations;
+	public static String MigrateSkeletalImplementationToInferfaceRefactoring_NoStaticMethods;
+
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
--- edu.cuny.citytech.defaultrefactoring/src/edu/cuny/citytech/defaultrefactoring/ui/refactorings/MigrateSkeletalImplementationToInterfaceRefactoring.java
+++ edu.cuny.citytech.defaultrefactoring/src/edu/cuny/citytech/defaultrefactoring/ui/refactorings/MigrateSkeletalImplementationToInterfaceRefactoring.java
@@ -1,8 +1,16 @@
 package edu.cuny.citytech.defaultrefactoring.ui.refactorings;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.NullChange;
@@ -23,7 +31,7 @@ public class MigrateSkeletalImplementationToInterfaceRefactoring extends
 	/**
 	 * The methods to refactor.
 	 */
-	private IMethod[] methods;
+	private Set<IMethod> methods;
 
 	/**
 	 * Creates a new refactoring with the given methods to refactor.
@@ -31,8 +39,9 @@ public class MigrateSkeletalImplementationToInterfaceRefactoring extends
 	 * @param methods
 	 *            The methods to refactor.
 	 */
-	public MigrateSkeletalImplementationToInterfaceRefactoring(IMethod[] methods) {
-		this.methods = methods;
+	public MigrateSkeletalImplementationToInterfaceRefactoring(
+			IMethod... methods) {
+		this.methods = new HashSet<IMethod>(Arrays.asList(methods));
 	}
 
 	/**
@@ -52,7 +61,70 @@ public class MigrateSkeletalImplementationToInterfaceRefactoring extends
 		final RefactoringStatus status = new RefactoringStatus();
 		// TODO Probably should make sure that the enclosing type implements an
 		// interface.
+		// TODO Can't be a static method (for now).
+		// TODO Can't be part of an annotation (at least for now; this should be
+		// checked on the declaring type).
+		// TODO No enum methods.
+		try {
+			pm.beginTask(
+					Messages.MigrateSkeletalImplementationToInferfaceRefactoring_CheckingPreconditions,
+					methods.size());
+
+			if (this.methods.isEmpty())
+				status.addFatalError(Messages.MigrateSkeletalImplementationToInferfaceRefactoring_MethodsNotSpecified);
+			else {
+				for (Iterator<IMethod> iterator = methods.iterator(); iterator
+						.hasNext();) {
+					IMethod method = iterator.next();
+
+					if (!method.exists()) {
+						removeMethod(
+								Messages.MigrateSkeletalImplementationToInferfaceRefactoring_MethodDoesNotExist,
+								method, iterator, status, pm);
+						iterator.remove();
+						pm.worked(1);
+					} else if (method.isBinary() || method.isReadOnly()) {
+						removeMethod(
+								Messages.MigrateSkeletalImplementationToInferfaceRefactoring_CantChangeMethod,
+								method, iterator, status, pm);
+						iterator.remove();
+						pm.worked(1);
+					} else if (!method.isStructureKnown()) {
+						removeMethod(
+								Messages.MigrateSkeletalImplementationToInferfaceRefactoring_CUContainsCompileErrors,
+								method, iterator, status, pm);
+					} else if (method.isConstructor()) {
+						removeMethod(
+								Messages.MigrateSkeletalImplementationToInferfaceRefactoring_NoConstructors,
+								method, iterator, status, pm);
+					} else if (method.getAnnotations().length > 0) {
+						removeMethod(
+								Messages.MigrateSkeletalImplementationToInferfaceRefactoring_NoAnnotations,
+								method, iterator, status, pm);
+					} else if (Flags.isStatic(method.getFlags())) {
+						removeMethod(
+								Messages.MigrateSkeletalImplementationToInferfaceRefactoring_NoStaticMethods,
+								method, iterator, status, pm);
+					}
+				}
+			}
+
+			if (this.methods.isEmpty())
+				status.addError(Messages.MigrateSkeletalImplementationToInferfaceRefactoring_PreconditionFailed);
+
+		} finally {
+			pm.done();
+		}
+
 		return status;
+	}
+
+	private static void removeMethod(String message, IMethod method,
+			Iterator<IMethod> iterator, final RefactoringStatus status,
+			IProgressMonitor pm) {
+		status.addWarning(MessageFormat.format(message, method.getElementName()));
+		iterator.remove();
+		pm.worked(1);
 	}
 
 	@Override
@@ -71,7 +143,7 @@ public class MigrateSkeletalImplementationToInterfaceRefactoring extends
 			pm.beginTask(
 					Messages.MigrateSkeletalImplementationToInferfaceRefactoring_CreatingChange,
 					1);
-			
+
 			return new NullChange(getName());
 		} finally {
 			pm.done();
