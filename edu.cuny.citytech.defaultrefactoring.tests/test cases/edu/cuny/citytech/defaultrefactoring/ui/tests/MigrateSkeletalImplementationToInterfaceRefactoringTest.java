628b2eaac701e155c305e3000d31c1d53e0e8e00
Md Arefin <arefin.shahrear@gmail.com>
2015-08-03 19:12:29 -0400

    Moved common stuff out to common refactoring test.

--- edu.cuny.citytech.defaultrefactoring.tests/test cases/edu/cuny/citytech/defaultrefactoring/ui/tests/MigrateSkeletalImplementationToInterfaceRefactoringTest.java
+++ edu.cuny.citytech.defaultrefactoring.tests/test cases/edu/cuny/citytech/defaultrefactoring/ui/tests/MigrateSkeletalImplementationToInterfaceRefactoringTest.java
@@ -3,27 +3,18 @@
  */
 package edu.cuny.citytech.defaultrefactoring.ui.tests;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.logging.Logger;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IBuffer;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.ui.tests.refactoring.Java18Setup;
-import org.eclipse.jdt.ui.tests.refactoring.RefactoringTest;
 import org.eclipse.ltk.core.refactoring.Refactoring;
-import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 
 import edu.cuny.citytech.defaultrefactoring.core.refactorings.MigrateSkeletalImplementationToInterfaceRefactoring;
+import edu.cuny.citytech.refactoring.common.tests.RefactoringTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -40,13 +31,7 @@ public class MigrateSkeletalImplementationToInterfaceRefactoringTest extends Ref
 	private static final Logger logger = Logger.getLogger(clazz.getName());
 
 	private static final String REFACTORING_PATH = "MigrateSkeletalImplementationToInterface/";
-
-	/**
-	 * The name of the directory containing resources under the project
-	 * directory.
-	 */
-	private static final String RESOURCE_PATH = "resources";
-
+	
 	/**
 	 * @param testSuite
 	 * @return
@@ -65,25 +50,7 @@ public class MigrateSkeletalImplementationToInterfaceRefactoringTest extends Ref
 	public MigrateSkeletalImplementationToInterfaceRefactoringTest(String name) {
 		super(name);
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.jdt.ui.tests.refactoring.RefactoringTest#getFileContents(java
-	 * .lang.String) Had to override this method because, since this plug-in is
-	 * a fragment (at least I think that this is the reason), it doesn't have an
-	 * activator and the bundle is resolving to the eclipse refactoring test
-	 * bundle.
-	 */
-	@Override
-	public String getFileContents(String fileName) throws IOException {
-		Path path = Paths.get(RESOURCE_PATH, fileName);
-		Path absolutePath = path.toAbsolutePath();
-		byte[] encoded = Files.readAllBytes(absolutePath);
-		return new String(encoded, Charset.defaultCharset());
-	}
-
+	
 	/**
 	 * @return the refactoringPath
 	 */
@@ -91,104 +58,15 @@ public class MigrateSkeletalImplementationToInterfaceRefactoringTest extends Ref
 	public String getRefactoringPath() {
 		return REFACTORING_PATH;
 	}
-
-	private void helperPass(String[] methodNames, String[][] signatures) throws Exception {
-		ICompilationUnit cu = createCUfromTestFile(getPackageP(), "A");
-		IType type = getType(cu, "A");
-		IMethod[] methods = getMethods(type, methodNames, signatures);
-
-		Refactoring refactoring = new MigrateSkeletalImplementationToInterfaceRefactoring(methods);
-
-		RefactoringStatus initialStatus = refactoring.checkInitialConditions(new NullProgressMonitor());
-		logger.info("Initial status: " + initialStatus);
-
-		RefactoringStatus finalStatus = refactoring.checkFinalConditions(new NullProgressMonitor());
-		logger.info("Final status: " + finalStatus);
-
-		assertTrue("Precondition was supposed to pass.", initialStatus.isOK() && finalStatus.isOK());
-		performChange(refactoring, false);
-
-		String expected = getFileContents(getOutputTestFileName("A"));
-		String actual = cu.getSource();
-		assertEqualLines(expected, actual);
-	}
-
-	/**
-	 * Check for failed precondition for a simple case.
-	 * 
-	 * @param methodNames
-	 *            The methods to test.
-	 * @param signatures
-	 *            Their signatures.
-	 * @throws Exception
-	 */
-	private void helperFail(String[] methodNames, String[][] signatures) throws Exception {
-		helperFail("A", null, null, null, methodNames, signatures);
-	}
-
-	private void helperFail(String innerTypeName, String[] methodNames, String[][] signatures) throws Exception {
-		helperFail("A", null, null, innerTypeName, methodNames, signatures);
-	}
-
-	private void helperFail(String outerMethodName, String[] outerSignature, String innerTypeName, String[] methodNames,
-			String[][] signatures) throws Exception {
-		helperFail("A", outerMethodName, outerSignature, innerTypeName, methodNames, signatures);
-	}
-
-	/**
-	 * Check for a failed precondition for a case with an inner type.
-	 * 
-	 * @param outerMethodName
-	 *            The method declaring the anonymous type.
-	 * @param outerSignature
-	 *            The signature of the method declaring the anonymous type.
-	 * @param methodNames
-	 *            The methods in the anonymous type.
-	 * @param signatures
-	 *            The signatures of the methods in the anonymous type.
-	 * @throws Exception
-	 */
-	private void helperFail(String outerMethodName, String[] outerSignature, String[] methodNames,
-			String[][] signatures) throws Exception {
-		helperFail("A", outerMethodName, outerSignature, null, methodNames, signatures);
-	}
-
-	private void helperFail(String typeName, String outerMethodName, String[] outerSignature, String innerTypeName,
-			String[] methodNames, String[][] signatures) throws Exception {
-		ICompilationUnit cu = createCUfromTestFile(getPackageP(), typeName);
-		IType type = getType(cu, typeName);
-
-		if (outerMethodName != null) {
-			IMethod method = type.getMethod(outerMethodName, outerSignature);
-			if (innerTypeName != null) {
-				type = method.getType(innerTypeName, 1); // get the local type
-			} else {
-				type = method.getType("", 1); // get the anonymous type.
-			}
-		} else if (innerTypeName != null) {
-			type = type.getType(innerTypeName); // get the member type.
-		}
-
-		IMethod[] methods = getMethods(type, methodNames, signatures);
-		assertFailedPrecondition(methods);
-	}
-
-	private void assertFailedPrecondition(IMethod... methods) throws CoreException {
-		Refactoring refactoring = new MigrateSkeletalImplementationToInterfaceRefactoring(methods);
-
-		RefactoringStatus initialStatus = refactoring.checkInitialConditions(new NullProgressMonitor());
-		logger.info("Initial status: " + initialStatus);
-
-		RefactoringStatus finalStatus = refactoring.checkFinalConditions(new NullProgressMonitor());
-		logger.info("Final status: " + finalStatus);
-
-		assertFailedPrecondition(initialStatus, finalStatus);
-	}
-
-	private static void assertFailedPrecondition(RefactoringStatus initialStatus, RefactoringStatus finalStatus) {
-		assertTrue("Precondition was supposed to fail.", !initialStatus.isOK() || !finalStatus.isOK());
+	
+	protected Refactoring getRefactoring(IMethod... methods) {
+		return new MigrateSkeletalImplementationToInterfaceRefactoring(methods);
 	}
 	
+	protected Logger getLogger() {
+		return logger;
+	}
+
 	private void helperFailLambdaMethod(String typeName, String lambdaExpression) throws Exception {
 		ICompilationUnit cu = createCUfromTestFile(getPackageP(), typeName);
 		IBuffer buffer = cu.getBuffer();
