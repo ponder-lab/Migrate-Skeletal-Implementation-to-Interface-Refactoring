ab0aa63d55889b405cc669a0d9f3a3cc212f2bd5
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2016-04-01 13:22:08 -0400

    This test doesn't seem correct.

--- edu.cuny.citytech.defaultrefactoring.tests/test cases/edu/cuny/citytech/defaultrefactoring/ui/tests/MigrateSkeletalImplementationToInterfaceRefactoringTest.java
+++ edu.cuny.citytech.defaultrefactoring.tests/test cases/edu/cuny/citytech/defaultrefactoring/ui/tests/MigrateSkeletalImplementationToInterfaceRefactoringTest.java
@@ -519,9 +519,9 @@ public class MigrateSkeletalImplementationToInterfaceRefactoringTest extends Ref
 	}
 
 	public void testDestinationInterfaceHierarchyWithInvalidClass2() throws Exception {
-		helperFail(new String[] { "m" }, new String[][] { new String[0] });
+		helperPass(new String[] { "m" }, new String[][] { new String[0] });
 	}
-
+	
 	public void testDestinationInterfaceHierarchyWithInvalidClass3() throws Exception {
 		helperFail(new String[] { "m" }, new String[][] { new String[0] });
 	}
