b1a223d37e005584b4fe1122c0d1efe3b8998756
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2016-04-01 12:21:20 -0400

    Testing a case where the called method is in both of the declaring type and destination interface hierarchies.

--- edu.cuny.citytech.defaultrefactoring.tests/test cases/edu/cuny/citytech/defaultrefactoring/ui/tests/MigrateSkeletalImplementationToInterfaceRefactoringTest.java
+++ edu.cuny.citytech.defaultrefactoring.tests/test cases/edu/cuny/citytech/defaultrefactoring/ui/tests/MigrateSkeletalImplementationToInterfaceRefactoringTest.java
@@ -594,6 +594,12 @@ public class MigrateSkeletalImplementationToInterfaceRefactoringTest extends Ref
 		helperPass(new String[] { "m" }, new String[][] { new String[0] });
 	}
 
+	public void testDeclaringTypeHierarchyWithInvalidClass6() throws Exception {
+		// here, we're testing a case where the called method is in both
+		// of the declaring type and destination interface hierarchies.
+		helperPass(new String[] { "m" }, new String[][] { new String[0] });
+	}
+
 	public void testDeclaringTypeHierarchyWithInvalidInterface() throws Exception {
 		helperFail(new String[] { "m" }, new String[][] { new String[0] });
 	}
